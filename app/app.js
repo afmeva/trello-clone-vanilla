3d22c74dfedeba233c2ed835e16ddbc599f76e2f
Andrés Medina <andresfelipe@andres-medina.local>
2018-04-09 21:13:43 -0400

    Add firebase as global and external to brocfile. Setup firebase.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
+import firebase from 'firebase'
+
 import { createApp } from '_core/virtual-dom'
 
 import view from '_components/form/form'
-// import createStore from './store/store'
 import store from './store/store'
 
 const app = createApp({
@@ -11,3 +12,15 @@ const app = createApp({
 })
 app.render()
 store.onChange(app.render)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD15J0oSJXODzVRPtQcHvtTLF78HNNFLGM",
+  authDomain: "trello-clone-30eb4.firebaseapp.com",
+  databaseURL: "https://trello-clone-30eb4.firebaseio.com",
+  projectId: "trello-clone-30eb4",
+  storageBucket: "trello-clone-30eb4.appspot.com",
+  messagingSenderId: "411006033442"
+});
+
+
+
