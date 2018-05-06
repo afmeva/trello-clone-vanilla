2da6f2333780fd9f25e163ebca8475a7ef6827c3
afmeva <afmeva@gmail.com>
2018-05-06 14:55:00 -0500

    Add board btn element

--- app/components/board-btn/board-btn.js
+++ app/components/board-btn/board-btn.js
@@ -1,7 +1,11 @@
 import { div, empty, button } from '_core/virtual-dom'
+import { link } from '_components/router/router'
 
 export default ({ name, url }) => (
-  button({
-    className: 'board-btn'
-  }, `${name} - ${url}`)
+  link({
+    className: 'board-btn',
+    },{
+    route: url,
+    content: name
+  })
 )
