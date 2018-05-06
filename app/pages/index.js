53574ffe053a556eb253f6c5afdf52905286fdb3
afmeva <afmeva@gmail.com>
2018-05-06 11:14:17 -0500

    Create index page

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,7 +2,9 @@ import { div, p, input, button } from '_core/virtual-dom'
 import newBoardBtn from '_components/new-board/new-board'
 
 export default () => {
-  return div({},
-    newBoardBtn()
+  return div({
+      className: 'index-page'
+    },
+    newBoardBtn(),
   )
 }
