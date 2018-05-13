6446991d281a82639f01a544dc4b8ec5cf3c43f2
afmeva <afmeva@gmail.com>
2018-05-13 11:48:22 -0500

    create index folder

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import '_core/firebase.config'
 import { createApp } from '_core/virtual-dom'
 import { div } from '_core/virtual-dom'
 import { router, route } from '_components/router/router'
-import index from '_pages/index'
+import index from '_pages/index/index'
 import store from '_store/store'
 
 const view = () => {
