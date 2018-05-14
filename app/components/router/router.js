b998a78f9b57f625a425f15eeb4f6fe0de67a823
afmeva <afmeva@gmail.com>
2018-05-14 09:45:06 -0500

    WIP: changing router

--- app/components/router/router.js
+++ app/components/router/router.js
@@ -2,8 +2,9 @@ import { div } from '_core/virtual-dom'
 import injectStore from '_store/inject-store'
 import chronicle from './chronicle';
 
-const URL_ROOT = '#'
+const URL_ROOT = '#/'
 const DEFAULT_ROUTE = '#index.html'
+const PARAM_REGEX = /:\w*/g
 const routes = {}
 
 const link = (attrs = {}, { route, content }) => {
@@ -16,23 +17,47 @@ const link = (attrs = {}, { route, content }) => {
 }
 
 const route = ({ route, component }) => {
-  routes[`${URL_ROOT}${route}`] = component
-  return component
+  const regex = getRegexp(route)
+
+  return {
+    route: `${URL_ROOT}${route}`,
+    regex,
+    component,
+  }
+}
+
+const getRegexp = (route) => {
+  let regexp = '';
+
+  const str = route.replace(PARAM_REGEX, '(.*)')
+  regexp = new RegExp(str)
+
+  return regexp
+}
+
+const getParams = (route) => {
+  let params = [...( route.match(PARAM_REGEX) || [] )]
+  return params
+}
+
+const findComponent = (routes, path) => {
+  const { component } = routes.find(({ route }) => route === path)
+  return  component
 }
 
 const router = injectStore((...args) => {
   // store is always last argument
   const state = args.pop().getState()
-  const children = args
+  const routes = args
 
-  const route = state.router.currentPath
-  const component = routes[route]
+  const path = state.router.currentPath
+  const component = findComponent(routes, path)
 
-  return component
-    ? typeof component === 'function'
-      ? component()
-      : component
-    : routes[DEFAULT_ROUTE]
+  console.log(getRegexp('board/:boardId/other/:otherId'))
+  console.log(getRegexp('board/other/'))
+  console.log(getParams('board/:boardId/other/:otherId'))
+  console.log(getParams('board/other/'))
+  return component 
 })
 
 export { link, router, route }
